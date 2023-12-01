bf59ede52100170828eaacad5b078ce316a32f78
Carlos Salinas <122329812+Salinasd25@users.noreply.github.com>
2023-12-01 11:07:40 -0400

    upd

--- config/settings.py
+++ config/settings.py
@@ -53,7 +53,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'config.urls'
+ROOT_URLCONF = get_env('ROOT_URLCONF', True)
 
 TEMPLATES = [
     {
