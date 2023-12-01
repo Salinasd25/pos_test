264dbbd173484fff43d83638d842a75f36d47285
Carlos Salinas <122329812+Salinasd25@users.noreply.github.com>
2023-12-01 10:58:13 -0400

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -103,7 +103,7 @@ DATABASES = {
     }
 }
 
-AUTH_USER_MODEL = 'pos_test.User'
+# AUTH_USER_MODEL = 'pos_test.User'
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
